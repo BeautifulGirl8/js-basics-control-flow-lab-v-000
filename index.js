41d64809fa2f27576ba1f8c09f743204273ac58c
Vicky  T <vttak9@gmail.com>
2018-01-18 06:15:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your code in this file!
+
+
+
+
+
+
+
+function vendingMachine (snackSelection, moneyInserted) {
+  if (validateSelection(snackSelection) === false) {
+    return 'Please select a valid snack.';
+  }
+ 
+  const price = getPrice(snackSelection);
+ 
+  if (price > moneyInserted) {
+    return `Please insert more to purchase ${snackSelection}.`;
+  }
+ 
+  const change = moneyInserted - price;
+ 
+  return `${snackSelection} dispensed. Your change is ${change}. Thank you!`;
+}
